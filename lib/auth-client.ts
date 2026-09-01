4f984e2f71dc24f27b0e669dee7c59727362d94d
ejqs <62688314+ejqs@users.noreply.github.com>
2026-09-01 21:05:01 +0800

    chore: add auth plugins

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,12 @@
-import { sentinelClient } from "@better-auth/infra/client";
+import { apiKeyClient } from "@better-auth/api-key/client";
+import { adminClient } from "better-auth/client/plugins"
+import { createAuthClient } from "better-auth/client"
+// import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient = createAuthClient({
 	// ... your existing config
 	plugins: [
-		// ... other plugins
-		sentinelClient()
+		apiKeyClient(),
+		adminClient()
 	]
 })
--- lib/auth.ts
+++ lib/auth.ts
@@ -3,9 +3,16 @@ import { drizzleAdapter } from '@better-auth/drizzle-adapter/relations-v2';
 import { db } from "./database";
 import * as schema from "@/db/schema/auth-schema";
 
+import { apiKey } from "@better-auth/api-key"
+import { admin } from "better-auth/plugins"
+
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
 		schema,
 	}),
+	plugins: [
+		apiKey(),
+		admin()
+	]
 });
