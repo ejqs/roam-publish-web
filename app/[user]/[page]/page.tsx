8e3f12468069a1069c079167f0375f7bda00b286
ejqs <62688314+ejqs@users.noreply.github.com>
2026-09-03 23:39:04 +0800

    feat: add initial pages

---
+++ app/[user]/[page]/page.tsx
@@ -0,0 +1,15 @@
+
+/**
+ * USER/PAGE PAGE
+ * TODOs:
+ * [ ] Allow for crawlers to index the page
+ * [ ] Research best practices for SEO
+ * [ ] View Counter
+ * [ ] Way to comment
+ * [ ] Add visibility settings (PUBLIC, UNLISTED, PRIVATE)
+ */
+export default function PagePage() {
+	return (
+		<div></div>
+	)
+}
